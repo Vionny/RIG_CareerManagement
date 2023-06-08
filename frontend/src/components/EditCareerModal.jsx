2e5bb4b0393d4685aa410cb520fade5b78eb24a6
Gratianestha <98179730+Gratianestha@users.noreply.github.com>
2023-06-08 17:34:03 +0700

    Update EditCareerModal.jsx

--- frontend/src/components/EditCareerModal.jsx
+++ frontend/src/components/EditCareerModal.jsx
@@ -9,14 +9,15 @@ const EditCareerModal = ({ staff }) => {
         <div className="w-16 h-10">
             <button  onClick={()=>window.my_modal_3.showModal()}>Edit</button>
             {/* <button>Edit</button> */}
-            <dialog id="my_modal_3" className="modal relative ">
-            <form method="dialog" className="modal-box">
-                <button htmlFor="my-modal-3" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                <div>
-                    <h3 className="font-bold text-lg">Hello! {staff}</h3>
-                </div>
-                <p className="py-4">Press ESC key or click on ✕ button to close</p>
-            </form>
+            <dialog id="my_modal_3" className="z-50">
+                {/* <form method="dialog" className="modal-box">
+                    <button htmlFor="my-modal-3" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    <div>
+                        <h3 className="font-bold text-lg">Hello! {staff}</h3>
+                    </div>
+                    <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                </form> */}
+                <h1 className="">testtttt</h1>
             </dialog>
         </div>
     )
