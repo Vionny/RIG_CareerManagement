bc105891ab0239a9b3fc7944894fdbc2ec4951d0
Gratianestha <98179730+Gratianestha@users.noreply.github.com>
2023-07-11 11:28:31 +0700

    edit ast

--- backend/Routes/UserRoutes.js
+++ backend/Routes/UserRoutes.js
@@ -1,12 +1,13 @@
 
 const express = require('express');
-const { getUser, insertCareerChoice, finalizeCareerChoice, getTeamMember, getAllUser } = require('../Controller/UserController');
+const { getUser, insertCareerChoice, finalizeCareerChoice, getTeamMember, getAllUser, updateAssistant } = require('../Controller/UserController');
 const router = express.Router()
 
 
 router.get('/getUser/:initial',getUser)
 router.post('/insertCareerChoice',insertCareerChoice)
 router.post('/finalizeCareerChoice',finalizeCareerChoice)
+router.post('/updateAssistant',updateAssistant)
 router.get('/getTeamMember/:roleid', getTeamMember)
 router.get('/getAllUser', getAllUser)
 
