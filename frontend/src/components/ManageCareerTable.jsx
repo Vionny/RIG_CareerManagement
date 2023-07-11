b2215639f110cc262d1ba34306d833ddff7d373f
Vionny <senvionny@gmail.com>
2023-07-11 16:55:45 +0700

    Manage Career Page Finished

--- frontend/src/components/ManageCareerTable.jsx
+++ frontend/src/components/ManageCareerTable.jsx
@@ -1,10 +1,17 @@
 "use client"
 import "@/app/globals.css"
+import axios from 'axios';
 import "@/components/Modals/Edit/EditCareerModal.jsx"
 import EditCareerModal from "@/components/Modals/Edit/EditCareerModal.jsx";
-import React from 'react';
+import React, { useState } from 'react';
+import SimpleInformationModal from "./Modals/Information/SimpleInformationModal";
 
-const ManageCareerTable = ({ staff }) => {
+const ManageCareerTable = ({ assistant , filtered }) => {
+  const [selectedAst,setSelectedAst]= useState()
+  const [decision, setDecision] = useState()
+  const [infoEditedModal,setInfoEditedModal] = useState(false)
+  const [showEditModal,setShowEditModal] = useState(false)
+
   const getBgColor = (option) => {
     if (option === 'tentative') {
       return 'badge-warning';
@@ -12,41 +19,81 @@ const ManageCareerTable = ({ staff }) => {
       return 'badge-success';
     } else if (option === 'not willing') {
       return 'badge-error';
-    } else if (option === 'unknown') {
+    } else if (option === 'unknown' || option === undefined || option ==='') {
       return 'badge-info';
     }
   };
 
+  const handleEditCareer = ()=>{
+
+    console.log(selectedAst)
+    if(decision == undefined|| decision== null) {
+      setDecision(selectedAst.careerchoice)
+    }
+    var data = {
+      initial: selectedAst.initial,
+      careerchoice: (decision ? decision : selectedAst.careerchoice)
+    };
+    console.log(data)
+
+    axios.patch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/updateAstCareerChoice`,data).then((res)=>{
+      if(res.data === 'Success'){
+        console.log(res.data)
+        setShowEditModal(false)
+        setInfoEditedModal(true)
+      }
+    })
+
+  }
+
+
   return (
-    <table className="table table-compact w-full">
-      <thead >
-        <tr>
-          <th>Initial</th>
-          <th>Name</th>
-          <th>Role</th>
-          <th>Career Option</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody >
-        {staff.map((staffItem, index) => (
-          <tr key={index}  >
-            <td>{staffItem.initial}</td>
-            <td>{staffItem.name}</td>
-            <td>{staffItem.role}</td>
-            <td>
-                <div className={`badge ${getBgColor(staffItem.careerOption)}`}>
-                    {staffItem.careerOption}
-                </div>
-            </td>
-            <td>
-                <EditCareerModal staff={staffItem.initial}/>
-            </td>
-            
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className="max-h-80 overflow-auto">
+    {showEditModal && (
+        <EditCareerModal
+          initial={selectedAst ? selectedAst.initial : ''}
+          currentChoice={decision ? decision : selectedAst.careerchoice}
+          setCurrentChoice={setDecision}
+          onConfirm={handleEditCareer}
+        />
+      )}
+      {infoEditedModal && (
+        <SimpleInformationModal
+            title = "Career Choice Updated !"
+            message = {"You have successfully updating " + selectedAst.initial +"'s career choice to "+decision+" !"}
+            onConfirm= {()=>{window.location.reload()}}/>)}
+        <table className="table table-compact w-full">
+          <thead>
+            <tr>
+              <th className="sticky top-0 py-4 text-center">Initial</th>
+              <th className="sticky top-0 py-4 text-center">Name</th>
+              <th className="sticky top-0 py-4 text-center">Role</th>
+              <th className="sticky top-0 py-4 text-center">Career Option</th>
+              <th className="sticky top-0 py-4 text-center">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {assistant.map((assistantItem, index) => (
+              <tr key={index}>
+                <td className="text-center">{assistantItem.initial}</td>
+                <td className="text-center">{assistantItem.assistantname}</td>
+                <td className="text-center">{assistantItem.rolename}</td>
+                <td className="text-center">
+                  <div className={`badge ${getBgColor(assistantItem.careerchoice)}`}>
+                    {assistantItem.careerchoice}
+                  </div>
+                </td>
+                <td className="text-center">
+                  <button className="btn btn-info btn-sm btn-outline font-bold  border-blue-400" onClick={()=>{
+                    setSelectedAst(assistantItem);
+                    setShowEditModal(true);
+                  }}>Edit</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
   );
 };
 
--- frontend/src/pages/Career/ManageCareerChoice.jsx
+++ frontend/src/pages/Career/ManageCareerChoice.jsx
@@ -1,92 +1,186 @@
+'use client'
 
-"use client"
-import {React, useState} from 'react';
-import "@/app/globals.css"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import "@/app/globals.css";
 import ManageCareerTable from '@/components/ManageCareerTable';
 import PieChart from '@/components/PieChart';
 import RoleStatisticTable from '@/components/RoleStatisticTable';
 
+function ManageCareerChoice() {
+    const [assistant, setAssistant] = useState([]);
+    const [keyword, setKeyword] = useState('');
+    const [selectedDivision, setSelectedDivision] = useState(null);
+    const [filteredData, setFilteredData] = useState([]);
+    const [filtered, setFiltered] = useState(false);
+    const [selectedOption, setSelectedOption] = useState("None");
+    const [roleStatistics, setRoleStatistics] = useState(null);
+    const [loadRoleStatistics, setLoadRoleStatistics] = useState(false);
+    const [divisions, setDivisions] = useState([]);
+    const [pieChartData, setPieChartData] = useState({});
+    const [loadPieChartData, setLoadPieChartData] = useState(false)
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    useEffect(() => {
+        axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/getAllUser`)
+        .then((res) => {
+            setAssistant(res.data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 
-function ManageCareerChoice(){
-    
-    const staff = [
-        { initial: 'GH22-1', name: 'Gratia Honestha', role: 'Assistant', careerOption: 'tentative' },
-        { initial: 'VA22-1', name: 'Vionny Audrey Sen', role: 'Assistant', careerOption: 'unknown' },
-        { initial: 'CH21-1', name: 'Chelsey', role: 'OP Man', careerOption: 'willing' },
-        { initial: 'JM22-1', name: 'Jeremy Loa', role: 'NA Staff', careerOption: 'not willing' },
-      ];
-      
-      let divisions = [
-        { name: "Division 1", description: "Description 1" },
-        { name: "Division 2", description: "Description 2" },
-        { name: "Division 3", description: "Description 3" },
-        // Add more divisions as needed
-    ];
+        axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/getAllDivision`)
+        .then((res) => {
+            setDivisions(res.data);
+            setSelectedDivision(res.data[0]);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 
-    let roleStat = [
-        { role: 'DBA Staff', slot: 4, filled: 4, willing: 2, notwilling: 0, tentative: 1, unknown: 1},
-    ]
+        axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/getRoleStatistics`)
+            .then((res) => {
+                setRoleStatistics(res.data);
+                console.log(res.data);
+                setLoadPieChartData(true);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            })
+            .finally(() => {
+                setLoadRoleStatistics(true);
+        });
+    }, []);
 
-   const [selectedDivision, setSelectedDivision] = useState(divisions[0]);
-   
+    useEffect(() => {
+        if (assistant) {
+        if (selectedOption === "None" && keyword === "") {
+            setFiltered(false);
+        } else if (selectedOption === "None" && keyword !== "") {
+            console.log("no selection option");
+            setFilteredData(
+            assistant.filter((assistant) => (
+                    assistant.initial.toLowerCase().includes(keyword.toLowerCase()) ||
+                    assistant.assistantname.toLowerCase().includes(keyword.toLowerCase()) ||
+                    assistant.rolename.toLowerCase().includes(keyword.toLowerCase())
+                ))
+            );
+            setFiltered(true);
+        } else if (selectedOption !== "None" && keyword !== "") {
+            console.log("filter and search");
+            setFiltered(true);
+            setFilteredData(
+            assistant.filter((assistant) => (
+                assistant.initial.toLowerCase().includes(keyword.toLowerCase()) ||
+                assistant.assistantname.toLowerCase().includes(keyword.toLowerCase()) ||
+                assistant.rolename.toLowerCase().includes(keyword.toLowerCase())
+            )).filter((assistant) => assistant.initial.includes(selectedOption))
+            );
+        } else {
+            setFiltered(true);
+            setFilteredData(assistant.filter((assistant) => assistant.initial.includes(selectedOption)));
+        }
+        }
+    }, [keyword, selectedOption, assistant]);
 
+    useEffect(() => {
+        if (roleStatistics && selectedDivision) {
+            var willing = roleStatistics
+            .filter((item) => item.divisionid.includes(selectedDivision.divisionid))
+            .reduce((accumulator, currentItem) => parseInt(accumulator) + parseInt(currentItem.willing_count), 0);
 
-    return(
-        <div className="bg-base-200 flex flex-col pl-10 pr-10 pt-5 justify-center w-full min-h-max">
-            <div className=" h-max">
+            var notWilling = roleStatistics
+            .filter((item) => item.divisionid.includes(selectedDivision.divisionid))
+            .reduce((accumulator, currentItem) => parseInt(accumulator) + parseInt(currentItem.not_willing_count), 0);
 
-                <article className="prose base mb-5">
-                    <h2>Career Choice Management</h2>
-                </article>
+            var tentative = roleStatistics
+            .filter((item) => item.divisionid.includes(selectedDivision.divisionid))
+            .reduce((accumulator, currentItem) => parseInt(accumulator) + parseInt(currentItem.tentative_count), 0);
+            
+            setPieChartData({
+                willing : willing,
+                notWilling : notWilling,
+                tentative : tentative
+            })
+            console.log(pieChartData);
+        }
+    }, [loadPieChartData, selectedDivision])
+        
+    if (!loadRoleStatistics||!loadPieChartData) {
+        return <div></div>;
+    } else {
+        return (
+        <div className="bg-base-200 flex flex-col pl-10 pr-10 pt-5 w-full min-h-screen">
+            <div className="h-max">
+            <article className="prose base mb-5">
+                <h2>Career Choice Management</h2>
+            </article>
 
-                <div className="bg-base-100 card shadow-xl  w-full flex-auto p-3">
-                    <h1 className="card-title mb-2">Career Choice List</h1>
-                    <hr className="border-1 border-black" />
-                    <div>
-                    <ManageCareerTable staff={staff} />
-
-                    </div>
+            <div className="bg-base-100 card shadow-xl w-full p-3" style={{ height: 470 }}>
+                <div className="navbar bg-base-100">
+                <div className="flex-1">
+                    <a className="btn btn-ghost normal-case text-xl">Career Choice List</a>
+                </div>
+                <div className="flex-none">
+                    <input
+                    type="text"
+                    placeholder="Search"
+                    className="input input-bordered w-full max-w-xs"
+                    value={keyword}
+                    onChange={(e) => setKeyword(e.target.value)}
+                    />
+                </div>
+                </div>
+                <hr className="border-1 border-black" />
+                <div className="p-7">
+                {assistant ? (
+                    <ManageCareerTable assistant={filtered ? filteredData : assistant} />
+                ) : null}
                 </div>
             </div>
-
-            <div className=" flex flex-row  gap-x-5 h-96 mt-8">
-                    <div className='card bg-base-100 w-2/5'>
-                        <div className='card-body'>
-                            <h2 className='card-title'>Career Choice Statistic</h2>
-                            <div className='flex flex-row justify-center items-center h-10'>
-                                {divisions.map((division, index) => (
-                                    <button
-                                    key={index}
-                                    onClick={() => setSelectedDivision(division)}
-                                    className= {`badge badge-ghost ${selectedDivision === division ? "badge bg-primary" : ""}`}
-                                    >
-                                    {division.name}
-                                    </button>
-                                ))}
-                                
-                                    
-                            
-                            </div>
-                            <div>
-                                <PieChart/>
-                            </div>
-                            
-                        </div>
-                    </div>
-                    <div className='card bg-base-100 w-3/5'>
-                        <div className='card-body'>
-                            <h2 className='card-title'>Role Statistic</h2>
-                            <RoleStatisticTable role={roleStat}/>
-                        </div>
-                    </div>
-                   
-
             </div>
-           
+
+            <div className="flex flex-row gap-x-5 mt-8" style={{ height: 470 }}>
+            <div className="card bg-base-100 w-2/5">
+                <div className="card-body">
+                <div className="card-title bg-base-100 mb-10">
+                    <div className="flex">
+                        <a className="btn btn-ghost normal-case text-xl">Career Choice Statistic</a>
+                    </div>
+                    <div className="dropdown dropdown-end">
+                        <label
+                        tabIndex={0}
+                        className={`btn btn-info m-1 ml-20 w-56 ${isDropdownOpen ? 'active' : ''}`}
+                        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                        >{selectedDivision? selectedDivision.divisionname : ""}</label>
+                        <ul tabIndex={0} className={`dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-96 max-h-64 overflow-y-scroll overflow-x-hidden flex-row flex ${ isDropdownOpen ? 'open' : '' }`} >
+                            {divisions && divisions.map((division, index) => (
+                            <li className='w-96 mr-4' key={index} onClick={() => {
+                                setSelectedDivision(division);
+                                setLoadPieChartData(true)
+                                setIsDropdownOpen(!isDropdownOpen)
+                            }}>
+                                <a className="h-14">{division.divisionname}</a>
+                            </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    {pieChartData &&(<PieChart key={`${pieChartData.willing}-${pieChartData.tentative}-${pieChartData.notWilling}`} data={pieChartData} />)}
+                </div>
+                </div>
+            </div>
+            <div className="card bg-base-100 w-3/5">
+                <div className="card-body">
+                <h2 className="card-title">Role Statistic</h2>
+                <RoleStatisticTable roleStatistics={roleStatistics} />
+                </div>
+            </div>
+            </div>
         </div>
-
-    )
-
+        );
+    }
 }
 
-export default ManageCareerChoice
+export default ManageCareerChoice;
