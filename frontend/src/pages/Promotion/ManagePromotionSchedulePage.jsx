574b7b4feba6ab426a1ba6302eddbecd2e66c37b
Vionny <senvionny@gmail.com>
2023-07-17 08:05:29 +0700

    add schedule manager page

---
+++ frontend/src/pages/Promotion/ManagePromotionSchedulePage.jsx
@@ -0,0 +1,15 @@
+"use client"
+import "@/app/globals.css"
+const axios = require("axios")
+
+const ManagePromotionSchedulePage = ()=>{
+
+    return (
+        <div className="bg-base-200 flex flex-col gap-2 pl-10 pr-10 py-3 w-full min-h-screen">
+            
+        </div>
+
+    )
+}
+
+export default ManagePromotionSchedulePage;
