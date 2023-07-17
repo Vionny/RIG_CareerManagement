62b191ec141e05655066087dc92fbe48817e0f88
Vionny <senvionny@gmail.com>
2023-07-17 15:26:43 +0700

    insert interview schedule

--- backend/Routes/TestRoutes.js
+++ backend/Routes/TestRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { BATestHandler, getBATestSchedule } = require('../Controller/TestController');
+const { BATestHandler, getBATestSchedule, getRoleNotInserted, getInterviewer, InterviewTestInputHandler } = require('../Controller/TestController');
 const router = express.Router();
 
 router.get('/getBATestSchedule/:semesterid',getBATestSchedule)
+router.get('/getRoleNotInserted/:semesterid/:initial',getRoleNotInserted)
+router.get('/getInterviewer',getInterviewer)
 
 router.post('/BATestHandler',BATestHandler)
-
+router.post('/InterviewTestInputHandler',InterviewTestInputHandler)
 module.exports = router;
--- frontend/src/pages/Promotion/ManagePromotionSchedulePage.jsx
+++ frontend/src/pages/Promotion/ManagePromotionSchedulePage.jsx
@@ -1,7 +1,7 @@
 "use client"
 import "@/app/globals.css"
 import BATestInputComponent from "@/components/BATestInputComponent"
-import TestScheduleTable from "@/components/Table/TestScheduleTable"
+import InterviewTestScheduleInput from "@/components/InterviewTestScheduleInput"
 const axios = require("axios")
 
 const ManagePromotionSchedulePage = ()=>{
@@ -9,7 +9,7 @@ const ManagePromotionSchedulePage = ()=>{
     return (
         <div className="bg-base-200 flex flex-col gap-2 pl-10 pr-10 py-3 w-full min-h-screen">
             <BATestInputComponent/>
-            <TestScheduleTable/>
+            <InterviewTestScheduleInput/>
         </div>
 
     )
